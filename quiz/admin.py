0b467fe26422453edc585309ee76d894e6a5b811
Ashish Subedi <ashishsubedi10@gmail.com>
2021-03-11 16:49:50 +0545

    Managed staff permission

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,11 +1,36 @@
 from django.contrib import admin
-from .models import  Problem, Option
+from .models import  Problem, Option, Quiz
 
-class OptionInline(admin.StackedInline):
+class OptionInline(admin.TabularInline):
     model = Option
-    extra = 2
+    extra = 4
+
+class ProblemInline(admin.TabularInline):
+    model = Problem
+    extra = 3
+    show_change_link = True
+
+    
+class QuizAdmin(admin.ModelAdmin):
+    readonly_fields = ['author']
+
+    inlines = [ProblemInline]
+    exclude = ['slug']
+
+    def get_form(self, request, obj=None, **kwargs):  
+        Quiz.author = request.user
+        return super().get_form(request, obj, **kwargs)
+        
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        obj.author_id = request.user.id
+        obj.last_modified_by = request.user
+        obj.save()
+
+admin.site.register(Quiz,QuizAdmin)
 
 class ProblemAdmin(admin.ModelAdmin):
     inlines = [OptionInline]
-    
+
+
 admin.site.register(Problem,ProblemAdmin)
--- quiz/models.py
+++ quiz/models.py
@@ -1,14 +1,47 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.text import slugify 
+
+User = get_user_model()
+
+
+class Quiz(models.Model):
+    author = models.ForeignKey(User,related_name='quiz',on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=255,default='')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True,blank=True,null=True)
+
+    class Meta:
+        ordering = ['-created']
+    
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.created)
+        super(Quiz, self).save(*args, **kwargs)
 
 
 class Problem(models.Model):
     question = models.TextField()
-    points = models.IntegerField(default = 1)
+    point = models.IntegerField(default = 1)
+    
+    quiz = models.ForeignKey('Quiz',related_name='problems',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.question}"
+    
+    def check_answer_exists(self,option):
+        return self.options.filter(id=option.id,is_answer=True).exists()
 
 class Option(models.Model):
     option = models.CharField(max_length=255)
     is_answer = models.BooleanField()
-    problem = models.ForeignKey(Problem,related_name='option',on_delete=models.CASCADE,blank=True,null=True)
+    problem = models.ForeignKey(Problem,related_name='options',on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.option} for {self.problem.question}"
 
-    
+class ScoreBoard(models.Model):
+    quiz = models.ForeignKey(Quiz,related_name='quiz',on_delete=models.DO_NOTHING)
