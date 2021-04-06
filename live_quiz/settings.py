c97fe6736eb4add93c901a80e3211981e238621b
Ashish Subedi <ashishsubedi10@gmail.com>
2021-04-06 14:47:34 +0545

    updated settings

--- live_quiz/settings.py
+++ live_quiz/settings.py
@@ -137,8 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =  BASE_DIR / 'staticfiles'
-
+STATIC_ROOT = config("STATIC_ROOT")
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static' ,
